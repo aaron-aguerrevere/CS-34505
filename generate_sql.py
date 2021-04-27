b71960282655f1c18453052ecedf7775819cfe5b
aaron a <aaguerrevere@legacy.com>
2021-04-27 15:46:43 -0500

    writing into new csv with clean results without extra commas or extra line breaks

---
+++ generate_sql.py
@@ -0,0 +1,37 @@
+# script to generate sql that will update header and footer urls in db
+############------------ IMPORTS ------------############
+import csv
+
+############------------ FUNCTIONS ------------############
+def generate_sql():
+    '''
+    UPDATE dbo.tblaffiliatesettings
+    SET headerurl = <headerurl>,
+        footerurl = <footerurl>,
+        artworkdeliveryoption = 3
+    WHERE affiliatesitename = <sitename>
+    '''
+    # target_columns Domain, Paper, Header, Footer
+
+    csvfile = open('gh.csv', newline='')
+
+    reader = csv.reader(csvfile, skipinitialspace=True)
+
+    with open('sql_scripts.cvs', 'w', newline='', encoding='utf-8') as result:
+    
+        writer = csv.writer(result, dialect='excel')
+
+        for row in reader:
+            writer.writerow([row[0]])
+            # writer.writerow(
+            #     f'UPDATE dbo.tblaffiliatesettings\n\
+            #     SET headerurl = {row[5]},\n\
+            #         footerurl = {row[6]},\n\
+            #         artworkdeliveryoption = 3\n\
+            #     WHERE affiliatesitename = {row[0]}'
+            # )
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == '__main__':
+    generate_sql()
